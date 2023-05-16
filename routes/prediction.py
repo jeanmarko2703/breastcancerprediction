49f0fcdbf02cefbcccca2b96c31c4f86efe7bad5
Jean Marko Aguirre <57963007+jeanmarko2703@users.noreply.github.com>
2023-05-15 19:38:43 -0500

    treatment machine learning model

--- routes/prediction.py
+++ routes/prediction.py
@@ -2,6 +2,7 @@ from http.client import HTTPException
 from telnetlib import STATUS
 from fastapi import APIRouter
 from schemas.prediction import Prediction
+from schemas.treatment import Treatment
 #import ML
 import pandas as pd
 import numpy as np  
@@ -45,6 +46,57 @@ def create_prediction(prediction: Prediction):
         result = 'BENIGNO'
     elif y_pred[0] == 1:
         result = 'MALIGNO'
+
+    else:
+        result = 'ERROR'
+
+
+
+    print(result)
+    return result
+
+
+@predictionRouter.post("/treatment")
+def create_prediction(PredictionTreatment: Treatment):
+    mnb = joblib.load('routes/breast_cancer_treatment_random_forest_model.joblib')
+
+
+    # data = {
+    #     'texture_mean' : [PredictionTreatment.AgeAtDiagnosis],
+    #     'perimeter_mean' : [prediction.perimeter_mean],
+    #     'smoothness_mean': [prediction.smoothness_mean],
+    #     'concave points_mean': [prediction.concave_points_mean],
+    #     'symmetry_mean' : [prediction.symmetry_mean],
+    #     'fractal_dimension_mean' : [prediction.fractal_dimension_mean],
+    # }
+    
+    data = {
+        'AgeAtDiagnosis' : [PredictionTreatment.AgeAtDiagnosis],
+        'Chemotherapy' : [PredictionTreatment.Chemotherapy],
+        'Cohort' : [PredictionTreatment.Cohort],
+        'ERStatus' : [PredictionTreatment.ERStatus],
+        'NeoplasmHistologicGrade' : [PredictionTreatment.NeoplasmHistologicGrade],
+        'HormoneTherapy' : [PredictionTreatment.HormoneTherapy],
+        'LymphNodesExaminedPositive' : [PredictionTreatment.LymphNodesExaminedPositive],
+        'MutationCount' : [PredictionTreatment.MutationCount],
+        'NottinghamPrognosticIndex' : [PredictionTreatment.NottinghamPrognosticIndex],
+        'OncotreeCode' : [PredictionTreatment.OncotreeCode],
+        'OverallSurvival(Months)' : [PredictionTreatment.OverallSurvival_Months],
+        'PRStatus' : [PredictionTreatment.PRStatus],
+        'RadioTherapy' : [PredictionTreatment.RadioTherapy],
+        'GeneClassifierSubtype' : [PredictionTreatment.GeneClassifierSubtype],
+        'TumorStage' : [PredictionTreatment.TumorStage],
+    }
+
+    data = pd.DataFrame(data)
+
+    #------ datos a predecir
+    y_pred=mnb.predict(data)
+
+    if y_pred[0] == 0:
+        result = 'MUERE'
+    elif y_pred[0] == 1:
+        result = 'SOBREVIVE'
 
     else:
         result = 'ERROR'
